7dfb916ffe40f786fdb1270b08e54417271e5ed0
Benjamin Austin <bnjmn80@gmail.com>
2021-09-03 14:44:58 -0600

    Convert function-components to class-based components

--- src/components/User.js
+++ src/components/User.js
@@ -1,7 +1,17 @@
+import { Component } from 'react';
+
 import classes from './User.module.css';
 
-const User = (props) => {
-  return <li className={classes.user}>{props.name}</li>;
-};
+// class-based component style
+class User extends Component {
+  render() {
+    return <li className={classes.user}>{this.props.name}</li>;
+  }
+}
+
+// function-component style
+// const User = (props) => {
+//   return <li className={classes.user}>{props.name}</li>;
+// };
 
 export default User;
