5639c774e4b59ca81f65d3d400eeee2710cda975
Benjamin Austin <bnjmn80@gmail.com>
2021-09-07 13:10:53 -0600

    Add Context to project and provide in App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import UserFinder from './components/UserFinder';
+import UsersContext from './store/users-context';
 
 import classes from './components/UserFinder.module.css';
 
+const DUMMY_USERS = [
+  { id: 'u1', name: 'Max'},
+  { id: 'u2', name: 'Manuel'},
+  { id: 'u3', name: 'Julie'}
+]
+
 function App() {
+  const usersContext = {
+    users: DUMMY_USERS
+  }
+  
   return (
-    <div className={classes.finder}>
+    <UsersContext.Provider value={usersContext} className={classes.finder}>
       <UserFinder />
-    </div>
+    </UsersContext.Provider>
   );
 }
 
